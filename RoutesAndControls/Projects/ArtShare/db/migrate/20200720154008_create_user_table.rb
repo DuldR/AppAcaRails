ac6d62e2bf9df64820162f759a0c47fb89c9c5ce
DuldR <gnjulaka@gmail.com>
2020-07-20 10:46:55 -0500

    Setup user table. Index on things that are used in where, having and order by as a rule of thumb.

---
+++ RoutesAndControls/Projects/ArtShare/db/migrate/20200720154008_create_user_table.rb
@@ -0,0 +1,10 @@
+class CreateUserTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.timestamps
+    end
+
+    add_index :users, :username
+  end
+end
