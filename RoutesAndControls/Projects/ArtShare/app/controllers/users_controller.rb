3341b8816762a957061ce6923c77ffd687ebc322
DuldR <gnjulaka@gmail.com>
2020-07-21 12:16:21 -0500

    Rested isn't a very good api interface

--- RoutesAndControls/Projects/ArtShare/app/controllers/users_controller.rb
+++ RoutesAndControls/Projects/ArtShare/app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
 
+    skip_before_action :verify_authenticity_token
 
 
     def index
-        render json: User.all
+        render json: params
+    end
+
+    def create
+
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: user.error.full_messages, status: :unprocessable
+        end
     end
 
 
