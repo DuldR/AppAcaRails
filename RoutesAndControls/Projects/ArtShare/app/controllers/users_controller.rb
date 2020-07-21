a0c45e3a5cb4fed57c0956ebd56ed8e5b4e2fdc1
DuldR <gnjulaka@gmail.com>
2020-07-21 12:01:22 -0500

    Initial route and controller ready!

---
+++ RoutesAndControls/Projects/ArtShare/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+
+
+    def index
+        render json: User.all
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
+    end
+
+end
