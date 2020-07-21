6008ddf7556410065acd0efbbb06876ae3f1a2a8
DuldR <gnjulaka@gmail.com>
2020-07-21 12:54:06 -0500

    Set the user updat to a user object and then use save to verify from there. If NG, respond with that objects error messages.

--- RoutesAndControls/Projects/ArtShare/app/controllers/users_controller.rb
+++ RoutesAndControls/Projects/ArtShare/app/controllers/users_controller.rb
@@ -17,6 +17,29 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def update
+
+        user = User.update(params[:id], user_params)
+
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+        # if User.update(params[:id], user_params)
+        #     render json: user.errors.full_messages, status: :un
+        # else
+        #     render json: user.errors.full_messages, status: :unprocessable_entity
+        # end
+        
+
+    end
+
     def destroy
         User.destroy(params[:id])
     end
