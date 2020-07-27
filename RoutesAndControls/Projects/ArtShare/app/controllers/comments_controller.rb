9d206a141484545861cc468087304b83ce496a71
DuldR <gnjulaka@gmail.com>
2020-07-27 13:54:27 -0500

    Comments working as intended.

--- RoutesAndControls/Projects/ArtShare/app/controllers/comments_controller.rb
+++ RoutesAndControls/Projects/ArtShare/app/controllers/comments_controller.rb
@@ -12,32 +12,17 @@ class CommentsController < ApplicationController
     end
 
     def index
-        # if comment_params[artwork_id].nil? == false
-        #     artwork = Artwork.find(comment_params[artwork_id])
-
-        # render json: Artwork.find(comment_params)
-        # if comment_params
-        # elsif comment_params.has_key?(:user_id)
-        #     comments = Comment.where(user_id: comment_params[:user_id])
-        # elsif comment_params.has_key?(:artwork_id)
-        #     comments = Comment.where(artwork_id: comment_params[:artwork_id])
-        # else
-        #     render json: comments.errors_full_messages, status: :bad_request
-        # end
-
-        # if comment_params.nil?
-        #     render json: {
-        #         error: "Enter a userid or artworkid",
-        #         status: 400
-        #     }, status: :bad_request
-        # end
-
-        if comment_params.permitted? == false
-            render json: {status: :bad_request}
+     
+   
+       
+        if comment_params.has_key?(:user_id)
+            comments = Comment.where(user_id: comment_params[:user_id])
+        elsif comment_params.has_key?(:artwork_id)
+            comments = Comment.where(artwork_id: comment_params[:artwork_id])
         end
 
-    
-    
+
+        render json: comments
     end
 
     private
