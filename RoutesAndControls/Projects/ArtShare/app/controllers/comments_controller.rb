5f50c60e3dc8fd6238fc3ca6cf40a7f0b848f5aa
DuldR <gnjulaka@gmail.com>
2020-07-27 13:51:43 -0500

    Controllers have a neat rescurefrom error raising if the user doesnt enter an id.

--- RoutesAndControls/Projects/ArtShare/app/controllers/comments_controller.rb
+++ RoutesAndControls/Projects/ArtShare/app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
 
     skip_before_action :verify_authenticity_token
 
+    rescue_from ActionController::ParameterMissing, with: :show_errors
+
 
     def create
     end
@@ -14,15 +16,26 @@ class CommentsController < ApplicationController
         #     artwork = Artwork.find(comment_params[artwork_id])
 
         # render json: Artwork.find(comment_params)
+        # if comment_params
+        # elsif comment_params.has_key?(:user_id)
+        #     comments = Comment.where(user_id: comment_params[:user_id])
+        # elsif comment_params.has_key?(:artwork_id)
+        #     comments = Comment.where(artwork_id: comment_params[:artwork_id])
+        # else
+        #     render json: comments.errors_full_messages, status: :bad_request
+        # end
 
-        if comment_params.has_key?(:user_id)
-            comments = Comment.where(user_id: comment_params[:user_id])
+        # if comment_params.nil?
+        #     render json: {
+        #         error: "Enter a userid or artworkid",
+        #         status: 400
+        #     }, status: :bad_request
+        # end
+
+        if comment_params.permitted? == false
+            render json: {status: :bad_request}
         end
 
-
-
-
-        render json: comments
     
     
     end
@@ -31,5 +44,9 @@ class CommentsController < ApplicationController
 
     def comment_params
         params.require(:comments).permit(:artwork_id, :user_id)
+    end
+
+    def show_errors
+        render json: {status: "Enter a userid or artworkid"}
     end
 end
