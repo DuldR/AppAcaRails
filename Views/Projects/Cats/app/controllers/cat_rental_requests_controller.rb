4e648b70213a463486ee154495cdc3ef35f284e8
DuldR <gnjulaka@gmail.com>
2020-08-09 22:25:00 -0500

    You can use all classes in passing an instance variable to a view:

--- Views/Projects/Cats/app/controllers/cat_rental_requests_controller.rb
+++ Views/Projects/Cats/app/controllers/cat_rental_requests_controller.rb
@@ -1,7 +1,26 @@
 class CatRentalRequestsController < ApplicationController
 
-    def index
-        @rentals = CatRentalRequest.all
-        render :index
+    def new
+        @rental = CatRentalRequest.new
+        @cats = Cat.all
+        render :new
+
+    end
+
+    def create
+        @rental = CatRentalRequest.new(rent_params)
+        if @rental.save
+            redirect_to cat_url(@rental.cat_id)
+        else
+            render :new
+        end
+    end
+
+
+    private
+
+    def rent_params
+        params.require(:cat_rental_request).permit(:cat_id, :start_date, :end_date, :status)
+
     end
 end
