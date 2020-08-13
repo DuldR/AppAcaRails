c8d1d27c5b7ff782427e4deceab2d160b039d66e
DuldR <gnjulaka@gmail.com>
2020-08-12 21:47:23 -0500

    Added before validation callback. Is onyl used BEFORE the validation lolgit add -Agit add -A Not when a new object is created.

--- Auth1/Homework/RailsAuth/auth/app/models/user.rb
+++ Auth1/Homework/RailsAuth/auth/app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+    validates :username, presence: true
+    validates :session_token, presence: true
+    validates :password_digest, presence: { message: "Password can't be blank" }, :length => { minimum: 6 }, allow_nil: true
+
+    before_validation :ensure_session_token
+
+
+    protected
+
+    def ensure_session_token
+
+        self.session_token ||= SecureRandom.hex(8)
+
+    end
 end
