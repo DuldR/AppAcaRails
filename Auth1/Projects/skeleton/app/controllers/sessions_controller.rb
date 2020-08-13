922f2ea5e4797cf7d27e9b1c25a177e7934afdde
DuldR <gnjulaka@gmail.com>
2020-08-13 16:18:02 -0500

    use the session to control the user

--- Auth1/Projects/skeleton/app/controllers/sessions_controller.rb
+++ Auth1/Projects/skeleton/app/controllers/sessions_controller.rb
@@ -1,3 +1,31 @@
 class SessionsController < ApplicationController
 
+    def new
+        render :new
+
+    end
+
+    def create
+        @user = User.new(session_params)
+
+        render json: @user
+        # if @user.save
+        #     redirect_to cats_url
+        # else
+        #     render :new
+        # end
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def session_params
+        params.require(:session).permit(:username, :password)
+
+    end
+    
 end
