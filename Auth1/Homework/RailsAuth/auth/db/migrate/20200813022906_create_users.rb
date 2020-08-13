f7bc7d4509c3526186d4f6cb9eafee807f1d6e52
DuldR <gnjulaka@gmail.com>
2020-08-12 21:31:51 -0500

    Don't make a rails app with Rails 6. Ensure it's 5.2.3 so you dont have to muck around with yarn and webpacker.

--- Auth1/Homework/RailsAuth/auth/db/migrate/20200813022906_create_users.rb
+++ Auth1/Homework/RailsAuth/auth/db/migrate/20200813022906_create_users.rb
@@ -1,4 +1,4 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
+class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :username, null: false
@@ -8,7 +8,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    add_index :users, :session_token
     add_index :users, :username
+    add_index :users, :session_token
   end
 end
