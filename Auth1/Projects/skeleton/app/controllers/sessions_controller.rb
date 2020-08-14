25251170551ee93910792484acc8bb12295253c7
DuldR <gnjulaka@gmail.com>
2020-08-14 13:30:32 -0500

    Added session token thats going to be the token for that session AND set instance vairabel @session to new token.

--- Auth1/Projects/skeleton/app/controllers/sessions_controller.rb
+++ Auth1/Projects/skeleton/app/controllers/sessions_controller.rb
@@ -7,11 +7,13 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by_credentials(params[:session][:username], params[:session][:password])
+        user.reset_session_token!
+        @session = user.session_token
 
         if user.nil?
             render json: "Wrong."
         else
-            render json: "Welcome back #{user.username}"
+            render json: "Welcome back #{user.username} and your token: #{user.session_token}"
         end
 
     end
