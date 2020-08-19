32e9049631939a1f638572d7703d0befdc83b7f4
Garrett <gnjulaka@gmail.com>
2020-08-19 11:33:35 -0500

    New user works. Need to ensure it sets the current user as the session.

--- Auth1/Projects/skeleton/app/controllers/users_controller.rb
+++ Auth1/Projects/skeleton/app/controllers/users_controller.rb
@@ -7,13 +7,20 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
+        @user.password = user_params[:password]
+        if @user.save!
+            redirect_to cats_url
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
     end
 
 
     private
 
     def user_params
-        params.require(:user).permit(:username)
+        params.require(:user).permit(:username, :password)
     end
 
 end
