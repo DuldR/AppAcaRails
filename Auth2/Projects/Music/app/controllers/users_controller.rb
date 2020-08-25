adf9ac6862e15db7427d78d5c81a8c0ae58f6c5d
DuldR <gnjulaka@gmail.com>
2020-08-25 11:55:02 -0500

    Sign up sheet working so far.

--- Auth2/Projects/Music/app/controllers/users_controller.rb
+++ Auth2/Projects/Music/app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
     def create
+        @user = User.new(user_params)
+        @user.password = user_params[:password]
+
+        if @user.save!
+            render :new
+        else
+            render :json => { :errors => @user.errors.full_messages }
+        end
     end
 
     def new
         @user = User.new
         render :new
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
     end
 end
