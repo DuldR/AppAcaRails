d618132d1ea6baf8767354b5f9015b95474679d0
DuldR <gnjulaka@gmail.com>
2020-08-28 14:08:00 -0500

    Once you log in it takes you to the home page and shows you the logout button.

--- Auth2/Projects/Music/config/routes.rb
+++ Auth2/Projects/Music/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:create, :new]
+  resources :users, only: [:create, :new, :show]
   resource :session, only: [:create, :new, :destroy]
-  resources :homes
+
+  resources :homes, only: [:index]
+
 end
