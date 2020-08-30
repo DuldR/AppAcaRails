46a8a5001bae1af145081d38e4c7733dd26ff19f
DuldR <gnjulaka@gmail.com>
2020-08-29 21:14:27 -0500

    Deleted homes and added bands. Fixed HTML to be 100% of window height

---
+++ Auth2/Projects/Music/app/controllers/bands_controller.rb
@@ -0,0 +1,30 @@
+class BandsController < ApplicationController
+
+    def index
+
+        render :index
+
+    end
+
+    def edit
+
+    end
+
+    def new
+
+    end
+
+    def update
+    end
+
+    def destroy
+
+    end
+
+    def show
+    end
+
+    def create
+
+    end
+end
--- Auth2/Projects/Music/config/routes.rb
+++ Auth2/Projects/Music/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create, :new, :show]
   resource :session, only: [:create, :new, :destroy]
-
+  resources :bands
+  
   resources :homes, only: [:index]
 
 end
