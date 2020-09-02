4299c11db347d92c21fa3a1c515a6e8f29a5661e
Garrett <gnjulak@gmail.com>
2020-09-02 16:03:16 -0500

    Albums table created with validations. When using associations ENSURE your foreign keys match precisely and you don't have to specify primary,foreign, class name etc.

---
+++ Auth2/Projects/Music/app/models/album.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  year       :integer          not null
+#  band_id    :integer          not null
+#  studio     :boolean          default(TRUE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Album < ApplicationRecord
+    validates :studio, inclusion: { in: [true, false] }
+    validates :title, :year, presence: true
+
+    belongs_to :band
+
+
+end
