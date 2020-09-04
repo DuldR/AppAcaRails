0f6572f1e656ae877ba90afadc5447347062ab83
DuldR <gnjulaka@gmail.com>
2020-09-04 15:42:00 -0500

    Track add and show working correctly.

--- Auth2/Projects/Music/app/controllers/albums_controller.rb
+++ Auth2/Projects/Music/app/controllers/albums_controller.rb
@@ -1,9 +1,5 @@
 class AlbumsController < ApplicationController
 
-    def index
-        @albums = Album.all
-        render :index
-    end
 
     def edit
         @album = Album.find(params[:id])
--- Auth2/Projects/Music/app/controllers/tracks_controller.rb
+++ Auth2/Projects/Music/app/controllers/tracks_controller.rb
@@ -1,9 +1,18 @@
 class TracksController < ApplicationController
 
     def new
+        @albums = Album.all
+        @album = Album.find(params[:album_id])
+        render :new
     end
     
     def create
+        @track = Track.new(track_params)
+        if @track.save!
+            redirect_to album_url(track_params[:album_id])
+        else
+            redirect_to new_album_track_url(track_params[:album_id])
+        end
     end
 
     def update
@@ -16,6 +25,14 @@ class TracksController < ApplicationController
     end
 
     def show
+        @track = Track.find(params[:id])
 
+        render :show
+
+
+    end
+
+    def track_params
+        params.require(:track).permit(:title, :ord, :bonus, :album_id, :lyrics)
     end
 end
