7980fe634456d58af772051617ea23fb61a1f78f
DuldR <gnjulaka@gmail.com>
2020-09-17 11:59:39 -0500

    Added shoulda matcher. You need to update the rails helper file though.

--- Testing/Notes/TestingApp/spec/models/capy_spec.rb
+++ Testing/Notes/TestingApp/spec/models/capy_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe TestModel, type: :model do
    describe "validations" do
-    it "should validate presence of name" do
-      capy = Capy.new(color: 'blue')
-      expect(capy.valid?).to be false
-    end
+    # it "should validate presence of name" do
+    #   capy = Capy.new(color: 'blue')
+    #   expect(capy.valid?).to be false
+    # end
+
+
+    #With a shoulda matcher
+
+    it { should validate_presence_of(:name) }
 
     it "should validate presence of color"
     it "should validate uniqueness of name"
