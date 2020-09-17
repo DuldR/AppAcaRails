32f886d1423e716e656d0e981b41f01ba1afe253
DuldR <gnjulaka@gmail.com>
2020-09-16 22:19:50 -0500

    Specify the bundle version when running install

--- Testing/Notes/CapybaraParty/CapybaraParty/spec/models/capy_spec.rb
+++ Testing/Notes/CapybaraParty/CapybaraParty/spec/models/capy_spec.rb
@@ -19,44 +19,33 @@ RSpec.describe Capy, type: :model do
   #   Error Messages
 
   # subject(:capy) { Capy.new(name: 'carlo', color: 'yellow') }
-  subject(:capy) { FactoryBot.build(:capy) }
 
-  describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:color) }
-    it { should validate_uniqueness_of(:name) }
+  # Test For:
+  # Validations
+  # Associations
+  # Class Methods
+  # Error Messages
 
-    it 'should validate color is not green' do
-      # green_capy = Capy.new(name: 'barbara', color: 'green')
-      # green_capy = FactoryBot.build(:capy, color: 'green')
-      green_capy = FactoryBot.build(:green_capy)
-      green_capy.valid?
-      expect(green_capy.errors[:color]).to eq(['cannot be green!!'])
+
+  describe "validations" do
+    it "should validate presence of name"
+    it "should validate presence of color"
+    it "should validate uniqueness of name"
+    it "should validate color is not green"
+  end
+
+  describe "associations" do
+    it "should have many parties"
+    it "should have many attendances"
+    it "should have many parties to attend"
+  end
+
+  describe "class methods" do
+    describe "::capys_of_the_rainbow" do
+      it "should return all capys of color rainbow"
     end
   end
 
-  describe 'associations' do
-    it { should have_many(:parties) }
-    it { should have_many(:attendances) }
-    it { should have_many(:parties_to_attend).through(:attendances) }
-  end
-
-  describe 'class methods' do
-    describe '::capys_of_the_rainbow' do
-      # it 'should return all capys of color rainbow' do
-      #   rainbow = Capy.create(name: 'rainbow', color: 'rainbow')
-      #   not_rainbow = Capy.create(name: 'not a rainbow', color: 'sparkly')
-      #
-      #   expect(Capy.capys_of_the_rainbow).to include(rainbow)
-      #   expect(Capy.capys_of_the_rainbow).not_to include(not_rainbow)
-      # end
-
-      it 'should return all capys of color rainbow' do
-        expect(Capy.capys_of_the_rainbow.where_values_hash).to eq({'color' => 'rainbow'})
-      end
-    end
-
-  end
 
 end
 
