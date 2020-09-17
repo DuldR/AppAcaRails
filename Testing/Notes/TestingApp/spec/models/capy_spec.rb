f60d24cddb0a9c22ce022a847a9b7d24ae996069
DuldR <gnjulaka@gmail.com>
2020-09-17 12:19:19 -0500

    Added party and attendance models. Added subhject so uniquness will work.

--- Testing/Notes/TestingApp/spec/models/capy_spec.rb
+++ Testing/Notes/TestingApp/spec/models/capy_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe TestModel, type: :model do
+RSpec.describe Capy, type: :model do
    describe "validations" do
     # it "should validate presence of name" do
     #   capy = Capy.new(color: 'blue')
     #   expect(capy.valid?).to be false
     # end
 
+    subject(:capy) { Capy.new(name: "Anus", color: "Brown") }
 
     #With a shoulda matcher
 
     it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:color) }
+    it { should validate_uniqueness_of(:name) }
 
     it "should validate presence of color"
     it "should validate uniqueness of name"
@@ -18,7 +21,9 @@ RSpec.describe TestModel, type: :model do
   end
 
   describe "associations" do
-    it "should have many parties"
+    it { should have_many(:parties) }
+    it { should have_many(:attendances) }
+    it { should have_many(:parties_to_attend) }
     it "should have many attendances"
     it "should have many parties to attend"
   end
