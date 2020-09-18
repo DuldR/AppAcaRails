e255f8c6f6e14977bfd80aa26a06ce0fdb0654d4
DuldR <gnjulaka@gmail.com>
2020-09-18 16:04:55 -0500

    Make sure to utilize it inside of describe blocks

--- Testing/Homework/Music/spec/models/user_spec.rb
+++ Testing/Homework/Music/spec/models/user_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe User, type: :model do
   describe "asssociations" do
   end
 
+  describe "class methods" do
+
+    #password_digest is using 'testpass' as hash
+    subject(:user) { User.new(email: "coolguy420@hotmail.com", id: 1, password_digest: "$2a$12$A4A5tGfH5sd8LLUgNKosxuS2xBTLNPhLjthBf7fxfDRIvWcj7h60a", session_token: "Di0qogACCK429C_TGnHNyg") }
+    
+    it "#is_password?" do
+      expect(user.is_password?("testpass")).to be true
+      expect(user.is_password?("wrongpass")).to be false
+    end
+
+
+  end
+
 end
