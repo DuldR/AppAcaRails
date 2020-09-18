1ad19de54742566a92d723889cff70b70ba46e2e
DuldR <gnjulaka@gmail.com>
2020-09-18 16:32:47 -0500

    Writing tests is fun!

--- Testing/Homework/Music/spec/models/user_spec.rb
+++ Testing/Homework/Music/spec/models/user_spec.rb
@@ -17,10 +17,34 @@ RSpec.describe User, type: :model do
     #password_digest is using 'testpass' as hash
     subject(:user) { User.new(email: "coolguy420@hotmail.com", id: 1, password_digest: "$2a$12$A4A5tGfH5sd8LLUgNKosxuS2xBTLNPhLjthBf7fxfDRIvWcj7h60a", session_token: "Di0qogACCK429C_TGnHNyg") }
     
-    it "#is_password?" do
-      expect(user.is_password?("testpass")).to be true
-      expect(user.is_password?("wrongpass")).to be false
+    describe "#is_password?" do
+      it "should return true if the correct password is given" do
+        expect(user.is_password?("testpass")).to be true
+        expect(user.is_password?("wrongpass")).to be false
+      end
     end
+
+    describe "#reset_session_token" do
+
+      it "should reset the user's session token" do
+        user.reset_session_token!
+        expect(user.session_token).not_to eq("Di0qogACCK429C_TGnHNyg")
+      end
+
+    end
+
+    describe "::find_by_credentials" do
+
+      it "should find the request user object" do
+        user.save!
+        expect(User.find_by_credentials("coolguy420@hotmail.com", "testpass")).to eq(user)
+      end
+
+    end
+
+
+
+
 
 
   end
