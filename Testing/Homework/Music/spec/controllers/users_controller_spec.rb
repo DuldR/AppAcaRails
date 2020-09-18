c8c023dbdf6f2f7da7f4613d4525399eb6b78085
DuldR <gnjulaka@gmail.com>
2020-09-18 17:23:22 -0500

    Controller testing complete

--- Testing/Homework/Music/spec/controllers/users_controller_spec.rb
+++ Testing/Homework/Music/spec/controllers/users_controller_spec.rb
@@ -17,11 +17,19 @@ RSpec.describe UsersController, :type => :controller do
         expect(flash.now[:errors]).to be_present
       end
 
-      it "validates that the password is at least 6 characters long"
+      it "validates that the password is at least 6 characters long" do
+        post :create, params: { user: { email: "coolguy420", password: "1" } }
+        expect(response).to render_template('new')
+        expect(flash.now[:errors]).to be_present
+    end
     end
 
     context "with valid params" do
-      it "redirects user to bands index on success"
+      it "redirects user to bands index on success" do
+        post :create, params: { user: { email: "coolguy420", password: "123456" } }
+        expect(response).to redirect_to(bands_url)
+      end
+
     end
   end
 end
