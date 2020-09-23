fec2ea6e40acaec288fa4a68ad8b70321691fdb4
Garrett <gnjulaka@gmail.com>
2020-09-22 21:04:13 -0500

    Wrote some model and controller tests

---
+++ Testing/Projects/MyGoals/spec/controllers/user_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+
+
+    describe "GET #new" do
+    end
+
+    describe "GET #index" do
+    end
+
+
+    describe "POST #create" do
+    end
+end
---
+++ Testing/Projects/MyGoals/spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe "validations" do
+    it "should validate presence of username"
+    it "should validate presence of password"
+    it "should validate uniquness of username"
+
+  end
+
+  describe "associations" do
+
+    it "should have many goals"
+
+  end
+
+  describe "class methods" do
+
+  end
+
+
+  
+end
