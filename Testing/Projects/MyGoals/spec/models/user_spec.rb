c07b28c90af0e88c0e64ec29322471d9e86a3e58
Garrett <gnjulaka@gmail.com>
2020-09-22 22:00:03 -0500

    Updated user spec.

--- Testing/Projects/MyGoals/spec/models/user_spec.rb
+++ Testing/Projects/MyGoals/spec/models/user_spec.rb
@@ -21,9 +21,7 @@ RSpec.describe User, type: :model do
    
 
     it { should validate_presence_of(:username) }
-    it "should validate presence of password"
     it { should validate_presence_of(:password_digest) }
-    it "should validate uniquness of username"
     it { should validate_uniqueness_of(:username) }
 
   end
