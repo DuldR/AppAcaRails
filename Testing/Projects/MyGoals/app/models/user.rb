6f528749e593d5812dab29778ddc334747fe005b
Garrett <gnjulaka@gmail.com>
2020-09-22 21:42:51 -0500

    Not really test driven, just making sure it works lol.

--- Testing/Projects/MyGoals/app/models/user.rb
+++ Testing/Projects/MyGoals/app/models/user.rb
@@ -1,3 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+require 'bcrypt'
+
+
 class User < ApplicationRecord
+
+    attr_reader :password
+
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, presence: { message: "Password can't be blank" }, :length => { minimum: 6 }, allow_nil: true
+
+
+    def self.find_by_credentials(username, password)
+
+        user = User.find_by(username: username)
+
+        return nil if user.nil?
+
+        if user.is_password?(password)
+            user
+        else
+            nil
+        end
+
+
+    end
+
+    def is_password?(pass)
+        check = BCrypt::Password.new(password_digest)
+        check.is_password?(pass)
+    end
+
+    
+
+
+    def password=(pass)
+        @password = pass
+        self.password_digest = BCrypt::Password.create(pass)
+    end
+
 
 end
--- Testing/Projects/MyGoals/spec/models/user_spec.rb
+++ Testing/Projects/MyGoals/spec/models/user_spec.rb
@@ -1,9 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
   describe "validations" do
-    it "should validate presence of username"
+    it { should validate_presence_of(:username) }
     it "should validate presence of password"
     it "should validate uniquness of username"
 
