c6c83ccffb86f6b6882d44dd04c3c68dc352c2fe
Garrett <gnjulaka@gmail.com>
2020-09-24 15:57:56 -0500

    A singular nil? returns false or true. That'll mess up your boolean logic.

--- Testing/Projects/MyGoals/app/controllers/users_controller.rb
+++ Testing/Projects/MyGoals/app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
 
+    def index
+        render :index
+    end
 
+    def show
+        @user = User.find_by(id: params[:id] )
+
+        if @user.nil? == false
+            render :show
+        else
+            redirect_to users_url
+        end
+
+    end
+
+    def create
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
