c4b449d471ed764815d58f9a5156d1430424e3a7
Garrett <gnjulaka@gmail.com>
2020-09-24 15:39:39 -0500

    Controller spec for user setup.

--- Testing/Projects/MyGoals/spec/controllers/user_controller_spec.rb
+++ Testing/Projects/MyGoals/spec/controllers/user_controller_spec.rb
@@ -4,13 +4,55 @@ RSpec.describe UsersController, type: :controller do
 
 
 
-    describe "GET #new" do
+    describe "GET #show" do
+        it "render the show template" do
+            user = User.new(username: "coolguy")
+            user.password = "123456"
+            user.save!
+
+            get :show, params: { id: user.id }
+            expect(response).to render_template(:show)
+        end
+
+        context "if the user does not exist" do
+            it "is not a success" do
+                begin
+                    get :show, params: { id: -1 }
+                rescue
+                    ActiveRecord::RecordNotFound
+                end
+
+                expect(response).not_to render_template(:show)
+            end
+
+        end
+        
     end
 
     describe "GET #index" do
+        it "renders the users index" do
+            get :index
+            expect(response).to be_success
+            expect(response).to render_template(:index)
+        end
     end
 
 
     describe "POST #create" do
+        context "with invalid params" do
+            it "renders the new template" do
+                post :create, params: { user: {username: "coolguy" } }
+                expect(response).to render_template(:new)
+
+            end
+        end
+
+        context "with valid params" do
+
+            it "redirects to new user url" do
+                post :create, params: { user: { username: "coolguy", password: "123456" } }
+                expect(response).to redirect_to(user_url(User.find_by(username: "coolguy")))
+            end
+        end
     end
 end
